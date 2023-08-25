7e63d7f9260ae22478b0a91d1b395ef474d89fa2
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-08-25 15:03:45 -0300

    aula25/08/2023

--- src/main/java/com/example/productbackend/resources/ProductController.java
+++ src/main/java/com/example/productbackend/resources/ProductController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.productbackend.dtos.ProductRequest;
+import com.example.productbackend.dtos.ProductResponse;
 import com.example.productbackend.entities.Product;
 import com.example.productbackend.services.ProductService;
 
@@ -43,7 +45,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product){
+    public ResponseEntity<ProductResponse> save(@RequestBody ProductRequest product){
         var savedProduct = this.service.save(product);
         URI location = ServletUriComponentsBuilder
 
@@ -51,7 +53,7 @@ public class ProductController {
 
                 .path("/{id}")
 
-                .buildAndExpand(savedProduct.getId())
+                .buildAndExpand(savedProduct.id())
 
                 .toUri();
         return ResponseEntity.created(location).body(savedProduct);
