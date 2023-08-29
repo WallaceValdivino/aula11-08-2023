75deef2a6694e8dda6fe311f14fddf8e0efdcde0
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-08-29 14:57:40 -0300

    aula29/08/2023

--- src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,14 +17,32 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFoundEXception(EntityNotFoundException e,
             HttpServletRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
+   
 
         StandardError error = new StandardError();
+        HttpStatus status = HttpStatus.NOT_FOUND;
         error.setStatus(status.value());
         error.setTimeStamp(Instant.now());
         error.setError("Resource not found");
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationErrors> validationErrorsEXception(MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+      
+        ValidationErrors error = new ValidationErrors();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        error.setStatus(status.value());
+        error.setTimeStamp(Instant.now());
+        error.setError("Validation Erros");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        e.getBindingResult().getFieldErrors().forEach( err -> error.addError(err.getDefaultMessage()));
         return ResponseEntity.status(status).body(error);
 
     }
